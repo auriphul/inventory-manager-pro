80a2cb6927cef1c4182cd4cad4b9d9e83fa8c6d4
Aurang Zeb <37955760+auriphul@users.noreply.github.com>
2025-06-23 15:59:49 +0500

    Render batch fields using table

--- templates/frontend/product-batch-archive.php
+++ templates/frontend/product-batch-archive.php
@@ -12,14 +12,14 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 ?>
-<div class="inventory-batch-info product-archive">
+<table class="inventory-batch-info product-archive">
 <?php foreach ( $displayed_fields as $field_key => $field ) : ?>
     <?php if ( isset( $batch_info[ $field_key ] ) && ! empty( $batch_info[ $field_key ] ) ) : ?>
         <?php $style = ! empty( $field['color'] ) ? ' style="color:' . esc_attr( $field['color'] ) . ';"' : ''; ?>
-        <div class="batch-info-field <?php echo esc_attr( $field_key ); ?>">
-            <span class="label"<?php echo $style; ?>><?php echo esc_html( $field['label'] ); ?>: </span>
-            <span class="value"<?php echo $style; ?>><?php echo esc_html( $batch_info[ $field_key ] ); ?></span>
-        </div>
+        <tr class="batch-info-field <?php echo esc_attr( $field_key ); ?>">
+            <td class="label"<?php echo $style; ?>><?php echo esc_html( $field['label'] ); ?></td>
+            <td class="value"<?php echo $style; ?>><?php echo esc_html( $batch_info[ $field_key ] ); ?></td>
+        </tr>
     <?php endif; ?>
 <?php endforeach; ?>
-</div>
+</table>
